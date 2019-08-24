60b019a92e4ac8bc6ce570960a8ee5e2bf5b4359
elsonlim <elsonlim7@gmail.com>
2019-08-24 17:40:34 +0800

    update header

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import { Members } from "./Components/Members";
 import { NameSelector } from "./Components/NameSelector";
 
 function App() {
-  const [memebrs, setMembers] = useState([
+  const [members, setMembers] = useState([
   ]);
   return (
     <div className="App">
       <Header />
-      <NameSelector members={memebrs} />
-      <Members members={memebrs} setMembers={setMembers} />
+      <NameSelector members={members} />
+      <Members members={members} setMembers={setMembers} />
     </div>
   );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,8 +3,8 @@ import { Navbar, NavbarBrand } from "reactstrap";
 export const Header = () => {
   return (
     <div id="main-header">
-      <Navbar color="dark" dark>
-        <NavbarBrand href="/">Who is next</NavbarBrand>
+      <Navbar color="primary" dark>
+        <NavbarBrand href="/">WHO IS NEXT</NavbarBrand>
       </Navbar>
     </div>
   );
