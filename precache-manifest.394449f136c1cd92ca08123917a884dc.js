4c987353dc43103e20d68c9798b5e12d71d2a2c5
elsonlim <elsonlim7@gmail.com>
2019-08-25 18:12:11 +0800

    Updates

---
+++ precache-manifest.394449f136c1cd92ca08123917a884dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d40356898c8fca91785ed51eb1f55a3",
+    "url": "/who-is-next/index.html"
+  },
+  {
+    "revision": "77dfdc9b8fade8d77530",
+    "url": "/who-is-next/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "edcd7ad436ba491205f6",
+    "url": "/who-is-next/static/css/main.582ca678.chunk.css"
+  },
+  {
+    "revision": "77dfdc9b8fade8d77530",
+    "url": "/who-is-next/static/js/2.99642aae.chunk.js"
+  },
+  {
+    "revision": "edcd7ad436ba491205f6",
+    "url": "/who-is-next/static/js/main.a0fd6679.chunk.js"
+  },
+  {
+    "revision": "1bf3a6e19d17757e0c36",
+    "url": "/who-is-next/static/js/runtime~main.fe17951a.js"
+  }
+]);
