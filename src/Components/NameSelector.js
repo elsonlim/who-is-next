97fd233b3e954076dd774388033ed3a1dbd49597
Elson Lim <elsonlim7@gmail.com>
2022-10-16 10:09:27 +0800

    Fix button colour

--- src/Components/NameSelector.js
+++ src/Components/NameSelector.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import Button from "../Commons/Button";
+// import Button from "../Commons/Button";
+import { Button } from "reactstrap";
 
 import "./NameSelector.css";
 export const getRandomiseNameList = (names) => {
